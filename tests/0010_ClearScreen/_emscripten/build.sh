1252fd4b22a91003032d2fb9faa05763c6310bbd
Jimmy <jimmy@jimmysoftware.com>
2018-12-13 01:44:42 +0700

    Clear Screen (Cmake, make, emscripten)

--- tests/0010_ClearScreen/_emscripten/build.sh
+++ tests/0010_ClearScreen/_emscripten/build.sh
@@ -1,2 +1,2 @@
-emcc ../src/main.cpp -O2 -I../../../SDL_gui -s USE_SDL=2 -s LEGACY_GL_EMULATION=1 -o sdl2.html
+emcc ../src/main.cpp -O2 -I../../../SDL/include -s USE_SDL=2 -s LEGACY_GL_EMULATION=1 -o build/sdl2.html
 
