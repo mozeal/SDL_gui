adf8ef2130c36962d67af6a5ae50e6dd2050fe91
mozeal <mozeal@gmail.com>
2018-12-20 21:51:30 +0700

    SDL_ttf implements HarfBuzz

---
+++ SDL2_ttf_HarfBuzz/Xcode/SDL2_ttf/SDL2_ttf.h
@@ -0,0 +1,12 @@
+//
+//  SDL2_ttf.h
+//  SDL2_ttf
+//
+//  Created by Jimmy on 26/3/2561 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDL2_ttf : NSObject
+
+@end
---
+++ SDL2_ttf_HarfBuzz/Xcode/SDL2_ttf/SDL2_ttf.m
@@ -0,0 +1,12 @@
+//
+//  SDL2_ttf.m
+//  SDL2_ttf
+//
+//  Created by Jimmy on 26/3/2561 BE.
+//
+
+#import "SDL2_ttf.h"
+
+@implementation SDL2_ttf
+
+@end
