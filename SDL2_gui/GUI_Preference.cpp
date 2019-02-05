50da3ea899d3d71085a6fafbadcaeec90dc25a3f
mozeal <mozeal@gmail.com>
2019-02-05 09:32:04 +0700

    .

---
+++ SDL2_gui/GUI_Preference.cpp
@@ -0,0 +1,9 @@
+//
+//  GUI_Preference.cpp
+//  GUI_App
+//
+//  Created by Panutat Tejasen on 5/2/2562 BE.
+//  Copyright © 2562 Jimmy Software Co., Ltd. All rights reserved.
+//
+
+#include "GUI_Preference.h"
